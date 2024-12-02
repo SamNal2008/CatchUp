7fd685c4dfcfd683dce8d283f2a40b5b2df80a8d
SamNal2008 <samy.nalbandian@ext.bpifrance.fr>
2024-12-02 21:06:26 +0100

    wip try to fix ios like delete behavior

--- components/molecules/FriendLine.tsx
+++ components/molecules/FriendLine.tsx
@@ -1,164 +1,180 @@
-import { useCheckIns } from "@/contexts/CheckIns.context";
-import { useContacts } from "@/contexts/Contact.context";
-import { useColorSchemeOrDefault } from "@/hooks/useColorScheme";
-import { useNotifications } from "@/hooks/useNotificatons";
-import { ContactModel } from "@/repositories";
-import { useNewCheckinInfo, useSetContactToCheckin } from "@/store/CheckinNote.store";
-import { SymbolView } from "expo-symbols";
-import { useEffect, useState } from "react";
-import { Image, Pressable, StyleSheet, View, Text } from "react-native";
+import {useCheckIns} from "@/contexts/CheckIns.context";
+import {useContacts} from "@/contexts/Contact.context";
+import {useNotifications} from "@/hooks/useNotificatons";
+import {ContactModel} from "@/repositories";
+import {useNewCheckinInfo, useSetContactToCheckin} from "@/store/CheckinNote.store";
+import {SymbolView} from "expo-symbols";
+import {useEffect, useState} from "react";
+import {Alert, Image, Pressable, StyleSheet, View} from "react-native";
 import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
-import { Colors } from "react-native/Libraries/NewAppScreen";
-import { PrimaryButton } from "../atoms/PrimaryButton";
-import { ThemedText } from "../atoms/ThemedText";
-import { CheckInToast } from "../organisms/CheckInToast";
-import { InitialImage } from "./InitialImage";
-import Reanimated, { SharedValue, useAnimatedStyle } from "react-native-reanimated";
-import { Palette } from "@/constants/design";
+import {PrimaryButton} from "@/components";
+import {ThemedText} from "../atoms/ThemedText";
+import {CheckInToast} from "../organisms/CheckInToast";
+import {InitialImage} from "./InitialImage";
+import Reanimated, {SharedValue, useAnimatedStyle} from "react-native-reanimated";
+import {Palette} from "@/constants/design";
 
 type RightActionProps = {
     onPress: () => void;
-    prog: SharedValue<number>;
+    progress: SharedValue<number>;
     drag: SharedValue<number>;
 }
-  
 
-const RightAction = ({drag, onPress, prog}: RightActionProps) => {
-    const theme = useColorSchemeOrDefault();
+
+const RightAction = ({drag, onPress, progress}: RightActionProps) => {
     const styleAnimation = useAnimatedStyle(() => {
-      const showRightProgress = prog.value > 0;
-      const appliedTranslation = drag.value + 50;
-  
-      return {
-        transform: [{ translateX: appliedTranslation }],
-        opacity: showRightProgress ? 1 : 0,
-        backgroundColor: showRightProgress ? Palette.RED : Colors[theme].background
-      };
+        return {
+            transform: [{translateX: drag.value + 50}],
+            opacity: progress.value,
+            backgroundColor: Palette.RED,
+            marginRight: -100,
+            width: 100
+        };
     });
-  
+
     return (
-      <Reanimated.View style={[styleAnimation]}>
-        <Pressable onPress={onPress} style={deleteStyles.deleteButton}>
-          <Text style={deleteStyles.deleteButtonText}>Delete</Text>
-        </Pressable>
-      </Reanimated.View>
+        <Reanimated.View style={[deleteStyles.container, styleAnimation]}>
+            <View style={deleteStyles.actionContainer}>
+                <Pressable onPress={onPress}>
+                    <SymbolView name="trash.fill" size={32} tintColor={Palette.WHITE}/>
+                </Pressable>
+            </View>
+        </Reanimated.View>
     );
-  }
+};
 
-  const deleteStyles = StyleSheet.create({
-    deleteButton: {
-      padding: 16,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    deleteButtonText: {
-      color: Palette.WHITE,
-      fontSize: 16,
-    },
-  });
 
-export const FriendLine = ({ contact }: { contact: ContactModel }) => {
-    const { deleteFriend, friends } = useContacts();
-    const { getLatestCheckInForContact } = useCheckIns();
-    const { askForNotificationPermission } = useNotifications();
-  
+const deleteStyles = StyleSheet.create({
+    container: {
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    actionContainer: {
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+export const FriendLine = ({contact}: { contact: ContactModel }) => {
+    const {deleteFriend, friends} = useContacts();
+    const {getLatestCheckInForContact} = useCheckIns();
+    const {askForNotificationPermission} = useNotifications();
+
     const [isOnDeleteMode, setIsOnDeleteMode] = useState<boolean>(false);
     const toggleDeleteFriend = () => {
-      setIsOnDeleteMode((prev) => !prev);
+        setIsOnDeleteMode((prev) => !prev);
     };
-  
+
     const [lastCheckedIn, setLastCheckedIn] = useState<Date | null>(null);
     const [reload, setReload] = useState<boolean>(false);
     const setContactToCheckin = useSetContactToCheckin();
     const {contactToCheckin} = useNewCheckinInfo();
-  
+
     const isCheckingOnContact = contactToCheckin?.id === contact.id;
-  
+
     useEffect(() => {
-      if (friends.length > 0) {
-        askForNotificationPermission();
-      }
+        if (friends.length > 0) {
+            askForNotificationPermission();
+        }
     }, []);
-  
+
+    const createThreeButtonAlert = () =>
+        Alert.alert('Alert Title', 'My Alert Msg', [
+            {
+                text: 'Ask me later',
+                onPress: () => console.log('Ask me later pressed'),
+            },
+            {
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+            },
+            {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ]);
+
     useEffect(() => {
-      if (!contact.id) {
-        return;
-      }
-      setLastCheckedIn(getLatestCheckInForContact(contact.id));
+        if (isOnDeleteMode) {
+            createThreeButtonAlert();
+        }
+    }, [isOnDeleteMode]);
+
+    useEffect(() => {
+        if (!contact.id) {
+            return;
+        }
+        setLastCheckedIn(getLatestCheckInForContact(contact.id));
     }, [contact.id, reload]);
-  
-    const confirmDelete = (promptResponse: string) => {
-      if (promptResponse.toLowerCase() === contact.firstName?.toLowerCase()) {
-        removeFriend();
-        return;
-      }
-      alert('Friend deletion cancelled');
-    }
-  
+
     const removeFriend = () => {
-      if (!contact.id) {
-        alert('Unable to delete friend without id');
-        return;
-      }
-      deleteFriend(contact.id);
+        if (!contact.id) {
+            alert('Unable to delete friend without id');
+            return;
+        }
+        deleteFriend(contact.id).then(r => {
+            setReload((prev) => !prev);
+        });
     }
-  
+
     const checkInOnFriend = () => {
-      if (!contact.id) {
-        alert('Unable to check in on friend without id');
-        return;
-      }
-      setContactToCheckin(contact);
+        if (!contact.id) {
+            alert('Unable to check in on friend without id');
+            return;
+        }
+        setContactToCheckin(contact);
     }
-  
+
     const hasAlreadyCheckedIn = !!lastCheckedIn;
-  
+
     const toDaysAgo = hasAlreadyCheckedIn ? Math.round((new Date().getTime() - lastCheckedIn?.getTime()) / (1000 * 3600 * 24)) : null;
     const hasCheckedInToday = hasAlreadyCheckedIn && toDaysAgo! < 1;
-  
+
     return (
-        <Swipeable renderRightActions={(progress, drag) => <RightAction drag={drag} prog={progress} onPress={toggleDeleteFriend} />}>
-        <View style={[styles.friendContainer]}>
-          <View style={styles.friendNameContainer}>
-            {contact.image ?
-              <Image source={contact.image} style={styles.friendImage} /> :
-              <InitialImage firstName={contact.firstName} lastName={contact.lastName} size={50}/>}
-            <View>
-              <ThemedText style={styles.friendName}>{contact.firstName}</ThemedText>
-              {hasAlreadyCheckedIn ?
-                <ThemedText type="subText">{hasCheckedInToday ? "Checked in today" : `Checked in ${toDaysAgo} days ago`}</ThemedText>
-                  :
-                <ThemedText type="subText">Never checked in yet !</ThemedText>
-              }
+        <Swipeable renderRightActions={(progress, drag) => <RightAction drag={drag} progress={progress}
+                                                                        onPress={toggleDeleteFriend}/>}>
+            <View style={[styles.friendContainer]}>
+                <View style={styles.friendNameContainer}>
+                    {contact.image ?
+                        <Image source={contact.image} style={styles.friendImage}/> :
+                        <InitialImage firstName={contact.firstName} lastName={contact.lastName} size={50}/>}
+                    <View>
+                        <ThemedText style={styles.friendName}>{contact.firstName}</ThemedText>
+                        {hasAlreadyCheckedIn ?
+                            <ThemedText
+                                type="subText">{hasCheckedInToday ? "Checked in today" : `Checked in ${toDaysAgo} days ago`}</ThemedText>
+                            :
+                            <ThemedText type="subText">Never checked in yet !</ThemedText>
+                        }
+                    </View>
+                </View>
+                <PrimaryButton disabled={hasCheckedInToday} title={hasCheckedInToday ? 'Come later' : 'Check In'}
+                               onPress={checkInOnFriend}/>
+                <CheckInToast checkedInContact={contact} isVisible={isCheckingOnContact}/>
             </View>
-          </View>
-          <PrimaryButton disabled={hasCheckedInToday} title={hasCheckedInToday ? 'Come later' : 'Check In'} onPress={checkInOnFriend} />
-          <CheckInToast checkedInContact={contact} isVisible={isCheckingOnContact}/>
-        </View>
-      </Swipeable>
+        </Swipeable>
     )
-  }
+}
 
 const styles = StyleSheet.create({
-    rightAction: { width: 50, height: 50, backgroundColor: 'purple' },
     friendNameContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         gap: 10,
-      },
-      friendName: {
+    },
+    friendName: {
         fontSize: 16,
         fontWeight: 'bold'
-      },
-      friendImage: {
+    },
+    friendImage: {
         width: 50,
         height: 50,
         borderRadius: 50,
-      },
-      friendContainer: {
+    },
+    friendContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         padding: 10,
-      },
+    },
 });
